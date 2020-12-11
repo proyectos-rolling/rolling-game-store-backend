89a77a58c425c6d707ad056ae03a4667d10ba144
valeria lopez <vnlopez07@gmail.com>
2020-12-11 17:16:23 -0300

    creo controlador login en userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const bcryptjs = require('bcryptjs');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 
 
 exports.index = async (req, res) => {
@@ -12,34 +12,54 @@ exports.index = async (req, res) => {
   }
 };
 
-exports.createUser = async (req,res) =>{
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()})
+exports.createUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  const { email, password } = req.body;
+  try {
+    //primero busco si existe
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: 'El usuario ya existe!!' });
     }
-    const {email, password} = req.body;
-    try{
-        //primero busco si existe
-        let user = await User.findOne({email});
-        if(user){ 
-            return res.status(400).json({msg:'El usuario ya existe!!'});
-        }
-        //creo
-        user = new User(req.body);
-        //Hacer el hash de la contraseña
-        const salt = await bcryptjs.genSalt(10);
-        user.password = await bcryptjs.hash(password,salt);
-        
+    //creo
+    user = new User(req.body);
+    //Hacer el hash de la contraseña
+    const salt = await bcryptjs.genSalt(10);
+    user.password = await bcryptjs.hash(password, salt);
 
-        //guardar en la db
-        await user.save();
-        res.json({msg: 'Usuario creado correctamente!!'})
-    }catch(error){
-        console.log(error);
-        res.status(400).json({msg:'hubo un error'})
-    }
+
+    //guardar en la db
+    await user.save();
+    res.json({ msg: 'Usuario creado correctamente!!' })
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: 'hubo un error' })
+  }
 }
 
+//login
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    //busco si existe
+    const user = await User.findOne({ email });
+
+    if (!user)
+      return res.status(400).json({ msg: "El mail no existe" });
+    const isMatch = await bcryptjs.compare(password, user.password)
+    if (!isMatch)
+      return res.status(400).json({ msg: "El password is incorrecto" });
+    console.log(user)
+    res.json({ msg: "Logueado correctamente" })
+  } catch (err) {
+    return res.status(500).json({ msg: err.message })
+  }
+}
 
 
 
